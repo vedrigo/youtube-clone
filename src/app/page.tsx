07f2fcfac35e13dd269edf8e1ab9001d23c5ccfb
Rodrigo Verissimo <rodrigovenancioverissimo@gmail.com>
2023-05-19 14:46:01 -0300

    style: add react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { MdMenu } from "react-icons/md";
+
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className='h-screen'>
+      <div className='container px-4 h-14 flex flex-row justify-between bg-gray-200'>
+        <div>
+          <div>
+            <MdMenu />
+          </div>
+          <div>logo</div>
+        </div>
+        <div>buscar</div>
+        <div>perfil</div>
+      </div>
+    </main>
+  );
 }
